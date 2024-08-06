e1b6aa4c0a10559d173ffeecaea64368c2ed4d99
GusFiveO <augustinlorain@gmail.com>
2024-08-06 16:22:53 +0200

    feat(tester.py): use a list of empire, millennium, odds to be able to test multiple millennium file

--- tester.py
+++ tester.py
@@ -5,32 +5,83 @@ from src.C3PO import C3PO
 EMPIRE_FOLDER = "./test/empire/"
 MILLENNIUM_FALCON_FOLDER = "./test/millennium-falcon/"
 
-expectedOddsDict = {
-    "empire1.json": 0,
-    "empire2.json": 0.81,
-    "empire3.json": 0.9,
-    "empire4.json": 1,
-    "empire5.json": 1,
-    "empireError.json": 1
-}
+testList = [
+    {
+        "empireFile":"empire1.json",
+        "millenniumFalconFile":"millennium-falcon1.json",
+        "odds": 0
+    },
+    {
+        "empireFile":"empire2.json",
+        "millenniumFalconFile":"millennium-falcon2.json",
+        "odds": 0.81
+    },
+    {
+        "empireFile":"empire3.json",
+        "millenniumFalconFile":"millennium-falcon3.json",
+        "odds": 0.9
+    },
+    {
+        "empireFile":"empire4.json",
+        "millenniumFalconFile":"millennium-falcon4.json",
+        "odds": 1
+    },
+    {
+        "empireFile":"empire5.json",
+        "millenniumFalconFile":"millennium-falcon5.json",
+        "odds": 0.9
+    },
+    {
+        "empireFile":"empire6.json",
+        "millenniumFalconFile":"millennium-falcon6.json",
+        "odds": 1
+    },
+    {
+        "empireFile":"empire7.json",
+        "millenniumFalconFile":"millennium-falcon7.json",
+        "odds": 1
+    },
+    {
+        "empireFile":"empire8.json",
+        "millenniumFalconFile":"millennium-falcon8.json",
+        "odds": 1
+    },
+    {
+        "empireFile":"empire9.json",
+        "millenniumFalconFile":"millennium-falcon9.json",
+        "odds": 0.81
+    },
+    {
+        "empireFile":"empireError.json",
+        "millenniumFalconFile":"millennium-falcon5.json",
+        "odds": 0
+    },
 
+]
+
+def runTest(millenniumFalconJsonFileName, empireJsonFileName, expectedOutput):
+        millenniumFalconJsonPath = MILLENNIUM_FALCON_FOLDER + millenniumFalconJsonFileName
+        try:
+            testC3PO = C3PO(millenniumFalconJsonPath)
+        except Exception as e:
+            print(f"{millenniumFalconJsonPath} TEST FAILED ❌: exception catched: {type(e).__name__} {e}")
+            return
+
+        try:
+            testedFilePath = EMPIRE_FOLDER + empireJsonFileName
+            output = testC3PO.giveMeTheOdds(testedFilePath)
+            if output != expectedOutput:
+                print(f"{testedFilePath} TEST FAILED ❌: expected odds: {expectedOutput}; obtained: {output}")
+            else:
+                print(f"{testedFilePath} TEST PASSED ✅")
+        except Exception as e:
+            print(f"{testedFilePath} TEST FAILED ❌: exception catched: {type(e).__name__} {e}")
+            return
 
 if __name__ == "__main__":
-        milleniumFalconJsonPath = MILLENNIUM_FALCON_FOLDER + 'millennium-falcon.json'
-        try:
-            testC3PO = C3PO(milleniumFalconJsonPath)
-        except Exception as e:
-            print(type(e).__name__, e)
-            exit()
+        for test in testList:
+            millenniumFalconJsonFileName = test["millenniumFalconFile"]
+            empireJsonFileName = test["empireFile"]
+            expectedOutput = test["odds"]
+            runTest(millenniumFalconJsonFileName, empireJsonFileName, expectedOutput)
 
-        for fileName in expectedOddsDict.keys():
-            try:
-                testedFilePath = EMPIRE_FOLDER + fileName
-                expectedOdds = expectedOddsDict[fileName]
-                odds = testC3PO.giveMeTheOdds(testedFilePath)
-                if odds != expectedOdds:
-                    print(f"{testedFilePath} TEST FAILED ❌: expected odds: {expectedOdds}; obtained: {odds}")
-                else:
-                    print(f"{testedFilePath} TEST PASSED ✅")
-            except Exception as e:
-                print(f"{testedFilePath} TEST FAILED ❌: exception catched: {type(e).__name__} {e}")
